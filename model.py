7608405f5b63a559d94f1f6a2b6b543fcd9f4fc7
Yijun Dong <31287569+dyjdongyijun@users.noreply.github.com>
2026-09-21 15:21:54 +0200

    Step 42: stack_encoder_layers

--- model.py
+++ model.py
@@ -338,8 +338,12 @@ def assemble_encoder_layer(x, layer_params, num_heads, src_mask):
     mhsa = encoder_layer_self_attention_sublayer(x, layer_params['w_q'], layer_params['w_k'], layer_params['w_v'], layer_params['w_o'], layer_params['attn_gamma'], layer_params['attn_beta'], num_heads, src_mask)
     return encoder_layer_feed_forward_sublayer(mhsa, layer_params['w1'], layer_params['b1'], layer_params['w2'], layer_params['b2'], layer_params['ffn_gamma'], layer_params['ffn_beta'])
 
-# Step 42 - stack_encoder_layers (not yet solved)
-# TODO: implement
+# Step 42 - stack_encoder_layers
+def stack_encoder_layers(x, encoder_layer_params_list, num_heads, src_mask):
+    # sequentially apply each encoder layer to the running hidden state and return the final tensor.
+    for layer_params in encoder_layer_params_list:
+        x = assemble_encoder_layer(x, layer_params, num_heads, src_mask)
+    return x
 
 # Step 43 - decoder_layer_masked_self_attention_sublayer (not yet solved)
 # TODO: implement
